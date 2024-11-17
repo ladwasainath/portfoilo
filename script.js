927270fc375a0aa2f52da981cda2873e41bb5572
sainath ladwa <134958378+ladwasainath@users.noreply.github.com>
2024-11-17 13:36:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+window.onscroll = () => {
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100)
+
+ScrollReveal({
+    distance: "80px",
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', {
+    origin: 'top'
+});
+ScrollReveal().reveal('.home-img, .portfolio-box, .content form', {
+    origin: 'bottom'
+});
+ScrollReveal().reveal('.home-content h1, .about-img', {
+    origin: 'left'
+});
+ScrollReveal().reveal('.home-content p, .aboutcontent', {
+    origin: 'right'
+});
+
+const typed = new Typed('.multiple-text', {
+    strings: [" a Student.", "a Web App Developer.", "an Android App Developer."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
